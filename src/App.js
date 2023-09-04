f70a9002b99cc56a2a09fddddb344b4cedfdcfb4
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2023-09-04 22:47:45 +0900

    todo-appの修正

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef} from "react";
+import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [todos, setTodos] = useState([{id: 1, name: "Todo1", completed: false }
+]);
+
+const todoNameRef = useRef();
+
+const handleAddTodo = () => {
+ const name = todoNamerRef.current.value;
+ if(name === "") return
+ setTodos((prevTodos) => {
+   return [...prevTodos, { id: uuidv4(), nmae: name, completed: false }];
+ });
+ todoNameRef.current.value = null;
+};
+
   return (
     <div>
     <h2>
          todo-app 
     </h2> 
-         <input tyoe="text"  />
-         <button>TODOの追加</button>
+         <TodoList todos={todos} />
+         <input tyoe="text" ref={todoNameRef} />
+         <button onClick={handleAddTodo}>TODOの追加</button>
          <button>TODOの削除</button>
-       </div>
+    </div>
   );
 }
 
