ca0a9644caeb3ab63c02a377edd095444d709fe4
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2023-09-10 20:05:50 +0900

    todo-appの修正

--- src/App.js
+++ src/App.js
@@ -4,17 +4,104 @@ import { useState, useRef} from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
 
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const App = () => {
+  const [todos, setTodos] = useState([])
+}
+
+const [todoTitle, setTodoTitle] = useState('')
+const [todoId, setTodoId] = useState(todos.length + 1 
+const [filteredTodos, setFilteredTodos] = useState([])
+
+const handleAddFormChanges = (e) => {
+  setTodoTitle(e.target.value)
+}
+
+const resetFormInput = () => {
+  setTodoTitle('')
+}
+const handleDetailFormChanges = (e) => {
+  setNewTitle(e.target.value)
+}
+
+const handleOpenDetailForm = ({ id, title, status, detail }) => {
+  setIsDetailtable(true)
+  setDetailId(id)
+  setNewTitle(title)
+}
+
+const handleCloseDetailForm = () => {
+  setIsDetailtable(false)
+  setDetailId()
+}
+
+const handleAddTodo = () => {
+setTodos([...todos, { id: todoId, title: todoTitle, status: done, detail: detail }])
+setTodoId(todoId + 1)
+setTodoTitle('')
+}
+
+const handleDeleteTodo = (targetTodo) => {
+ setTodos(todos.filter((todo) => todo !== targetTodo))
+
+ const handleDetailTodo = () => {
+  const newTodos = todos.map((todo) => ({ ...todo }))
+
+  setTodos(() =>
+    newTodos.map((todo) =>
+      todo.id === detailId ? { ...todo, title: newTitle } : todo
+    )
+  )
+  setNewTitle('')
+  handleCloseDetailForm()
+  setDetailId()
+}
+
+const handleStatusChange = ({ id }, e) => {
+  const newTodos = todos.map((todo) => ({ ...todo }))
+
+  setTodos(
+    newTodos.map((todo) =>
+      todo.id === id ? { ...todo, status: e.target.value } : todo
+    )
+  )
+}
+
+useEffect(() => {
+  const filteringTodos = () => {
+    switch (filter) {
+      case 'notStarted':
+        setFilteredTodos(todos.filter((todo) => todo.status === 'notStarted'))
+        break
+      case 'inProgress':
+        setFilteredTodos(todos.filter((todo) => todo.status === 'inProgress'))
+        break
+      case 'done':
+        setFilteredTodos(todos.filter((todo) => todo.status === 'done'))
+        break
+      default:
+        setFilteredTodos(todos)
+    }
+  }
+  filteringTodos()
+}, [filter, todos])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
 function App() {
-  const [todos, setTodos] = useState([{id: 1, name: "Todo1", completed: false }
+  const [todos, setTodos] = useState([{id: 1, title: "Todo1", status: done, detail: detail }
 ]);
 
 const todoNameRef = useRef();
 
 const handleAddTodo = () => {
- const name = todoNamerRef.current.value;
+ const name = todoNameRef.current.value;
  if(name === "") return
  setTodos((prevTodos) => {
-   return [...prevTodos, { id: uuidv4(), nmae: name, completed: false }];
+   return [...prevTodos, { id: uuidv4(), title: title, status: done, detail: detail }];
  });
  todoNameRef.current.value = null;
 };
@@ -25,9 +112,35 @@ const handleAddTodo = () => {
          todo-app 
     </h2> 
          <TodoList todos={todos} />
-         <input tyoe="text" ref={todoNameRef} />
+         <input tyoe="text" lavel= "タイトル"  
+        value={todoTitle}
+        onChange={handleAddFormChanges}
+         ref={todoNameRef} />
+         <button onClick={handleAddTodo}>TODOの作成</button>
          <button onClick={handleAddTodo}>TODOの追加</button>
-         <button>TODOの削除</button>
+         <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+        <option value="all">すべて</option>
+        <option value="notStarted">未着手</option>
+        <option value="inProgress">作業中</option>
+        <option value="done">完了</option>
+      </select>
+         <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            <select
+              value={todo.status}
+              onChange={(e) => handleStatusChange(todo, e)}
+            >
+              <option value="notStarted">未着手</option>
+              <option value="inProgress">作業中</option>
+              <option value="done">完了</option>
+            </select>
+            <button onClick={() => handleOpenEditForm(todo)}>TODOの詳細</button>
+            <button onClick={() => handleDeleteTodo(todo)}>TODOの削除</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
